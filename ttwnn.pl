1e27e130fe9a73e1216514b9d422e8c88fad6066
Ian Dunham <dunham@ebi.ac.uk>
2013-08-16 16:56:22 +0100

    fixed bug with rsid split

--- ttwnn.pl
+++ ttwnn.pl
@@ -193,16 +193,16 @@ if (defined $file){
     if ($format =~ /rsid/){
         while (<$fh>){
             chomp;
-            my @rsid = split /:/, $_;
-            $rsid = pop @$rsid; # take the last one for want of a better idea.
+            my @rsid = split /\:/, $_;
+            $rsid = pop @rsid; # take the last one for want of a better idea.
             push @snps, $rsid;
         }
     }
     elsif ($format =~ /ian/){
         while (<$fh>){
             my ($chr, $beg, $end, $rsid, undef) = split "\t", $_;
-            my @rsid = split /:/, $rsid; # to deal with multiple rsids
-            $rsid = pop @$rsid; # take the last one for want of a better idea. Can't take all as they are the same thing.
+            my @rsid = split /\:/, $rsid; # to deal with multiple rsids
+            $rsid = pop @rsid; # take the last one for want of a better idea. Can't take all as they are the same thing.
             push @snps, $rsid;
         }
     }
@@ -253,6 +253,7 @@ else{
     warn "No SNPs given, so running for example on Pulmonary function set from the GWAS catalogue.";
     @snps = qw(rs2865531 rs2395730 rs12914385 rs11168048 rs1529672 rs357394 rs13147758 rs3769124 rs2647044 rs12504628 rs1541374 rs2869967 rs1928168 rs3094548 rs3867498 rs9978142 rs4762767 rs6903823 rs11172113 rs9310995 rs2571445 rs2070600 rs11727189 rs3734729 rs2906966 rs1036429 rs16909898 rs3995090 rs12477314 rs2544527 rs2284746 rs993925 rs2277027 rs1344555 rs1455782 rs2855812 rs2838815 rs11001819 rs12716852 rs2798641 rs4129267 rs7068966 rs12899618 rs153916 rs1551943 rs730532 rs1980057 rs3820928 rs2036527 rs10516526 rs2857595 rs3817928 rs310558 rs808225 rs12447804);
 }
+
 
 # Check we have enough SNPs
 if (scalar @snps < 20){
